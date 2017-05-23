78246233dd7d4eda4ebdb6bfd40ac47c78d7ef0f
Rishab Bokaria <rishab.bokaria@ril.com>
2017-05-23 19:41:48 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+const logFile = 'server.log';
+const year = new Date().getFullYear();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('currentYear', () => {
+    return year;
+});
+hbs.registerHelper('capitalize', (text) => {
+    return text.toUpperCase();
+});
+
+var app = express();
+app.set('view engine', hbs);
+app.listen(3000, () => {
+    console.log('Server is up on port 3000!');
+});
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var logText = `${now}:- ${request.method}-----${request.url}`
+    fs.appendFile(logFile, logText + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log file.');
+        }
+    });
+    console.log(logText);
+    next();
+});
+/*app.use((request, response, next) => {
+    response.render('maintainence.hbs');
+});*/
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (request, response) => {
+    response.render('home.hbs', {
+        pTitle: 'Home',
+        pMessage: 'Welcome to our home page!'
+    });
+});
+app.get('/about', (request, response) => {
+    response.render('about.hbs', {
+        pTitle: 'About',
+        pMessage: 'Happy to tell you about us.'
+    });
+});
+app.get('/bad', (request, response) => {
+    response.send({
+        error: 'unable to serve request'
+    });
+});
